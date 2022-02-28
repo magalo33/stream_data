62b0ffa047b876badba9bc8b1d592a4a65c6a990
unknown <miguellondonogarzon@gmail.com>
2022-02-28 13:02:39 -0500

    Ajusta tiempo de envio

--- stream-data/src/main/java/com/sophos/streamdata/controller/APIController.java
+++ stream-data/src/main/java/com/sophos/streamdata/controller/APIController.java
@@ -74,6 +74,7 @@ public class APIController {
                                     while((datoEnvio + "\n").getBytes() == null){
                                         System.out.println(datoEnvio+"<----cargando");
                                     }
+                                    Thread.sleep(0,001);
                                     outputStream.write((datoEnvio + "\n").getBytes());                            
                                     outputStream.flush();
                                     indice+=1;
