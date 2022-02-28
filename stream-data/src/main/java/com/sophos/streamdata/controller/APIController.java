16b6a6a8a5069d9c144376b140a54b6c8fa3a741
unknown <miguellondonogarzon@gmail.com>
2022-02-28 09:03:47 -0500

    ajuste excepciones

--- stream-data/src/main/java/com/sophos/streamdata/controller/APIController.java
+++ stream-data/src/main/java/com/sophos/streamdata/controller/APIController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+//http://190.27.52.244:9192/stream-data/api/stream/data
 
 @RequestMapping("/api/stream")
 @RestController
@@ -44,38 +45,39 @@ public class APIController {
                         outputStream.write((datoEnvio + "\n").getBytes());
                         outputStream.flush();
                     } else {
-                        try{}catch(Exception e){
-                            System.out.println("Error controlado "+e.toString());
-                        }
-                        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\nINICIANDO ENVIO\n");
-                        Calendar cal0 = Calendar.getInstance();
-                        long t1 = cal0.getTimeInMillis();
-                        
-                        for (int i = 0; i < sf.getListaDataEntrada().size(); i++) {
-                            double d = sf.getListaDataEntrada().get(i);
-                            String etiqueta = "IU";
-                            if (i == 0) {
-                                etiqueta = "IO";
-                            } else if ((i + 1) == sf.getListaDataEntrada().size()) {
-                                etiqueta = "IC";
+                        try{
+                            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\nINICIANDO ENVIO\n");
+                            Calendar cal0 = Calendar.getInstance();
+                            long t1 = cal0.getTimeInMillis();
+
+                            for (int i = 0; i < sf.getListaDataEntrada().size(); i++) {
+                                double d = sf.getListaDataEntrada().get(i);
+                                String etiqueta = "IU";
+                                if (i == 0) {
+                                    etiqueta = "IO";
+                                } else if ((i + 1) == sf.getListaDataEntrada().size()) {
+                                    etiqueta = "IC";
+                                }
+                                Calendar cal = Calendar.getInstance();
+                                long t = cal.getTimeInMillis();
+                                String ping = "\"" + d + "\"";
+                                String datoEnvio = cabeceraXml.concat(iuXml.replace("[ETIQUETA]", etiqueta).replace("[TIME]", (t + "")).replace("[VALUE]", ping));
+                                System.out.println("enviando " + datoEnvio);
+                                outputStream.write((datoEnvio + "\n").getBytes());                            
+                                outputStream.flush();
                             }
-                            Calendar cal = Calendar.getInstance();
-                            long t = cal.getTimeInMillis();
-                            String ping = "\"" + d + "\"";
-                            String datoEnvio = cabeceraXml.concat(iuXml.replace("[ETIQUETA]", etiqueta).replace("[TIME]", (t + "")).replace("[VALUE]", ping));
-                            System.out.println("enviando " + datoEnvio);
-                            outputStream.write((datoEnvio + "\n").getBytes());                            
-                            outputStream.flush();
-                        }
-                        sf.setListacargada(false);
-                        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\nFIN ENVIO\n");
-                        Calendar cal2 = Calendar.getInstance();
-                        long t2 = cal2.getTimeInMillis();
-                        long nt= (t2-t1)/1000;
-                        System.out.println("tiempo de ejecución "+nt);
+                            sf.setListacargada(false);
+                            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\nFIN ENVIO\n");
+                            Calendar cal2 = Calendar.getInstance();
+                            long t2 = cal2.getTimeInMillis();
+                            long nt= (t2-t1)/1000;
+                            System.out.println("tiempo de ejecución "+nt);
+                        }catch(Exception e){
+                            //ystem.out.println("Error controlado "+e.toString());
+                        }                        
                     }
                     }catch(Exception e){
-                        System.out.println("Error técnico "+e.toString());
+                        //System.out.println("Error técnico "+e.toString());
                     }                    
                 }
             }
